24c4f039f52ed34ec2152f49e7023c30ac5fb3db
ktyoung <ktyoung0507@gmail.com>
2023-09-04 11:59:15 +0900

    chap3-3. props, useState

--- react-native-component/src/App.js
+++ react-native-component/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 import MyButton from "./components/MyButton";
+import Counter from "./components/Counter";
 
 const App = () => {
   return (
@@ -12,10 +13,31 @@ const App = () => {
         justifyContent: "center",
       }}
     >
-      <Text style={{ fontSize: 30, marginBottom: 10 }}>
-        My Button Component
-      </Text>
-      <MyButton />
+      <Text style={{ fontSize: 30, marginBottom: 10 }}>Props</Text>
+      {/* 1. 부모 → 자식 */}
+      <MyButton
+        title="Button"
+        onPress={() => {
+          alert("props");
+        }}
+      />
+      {/* 2. 컴포넌트 태그 사이의 값 전달 */}
+      <MyButton
+        title="Button"
+        onPress={() => {
+          alert("clildren");
+        }}
+      >
+        Chlidren Props
+      </MyButton>
+      {/* 3. defaultProps */}
+      <MyButton
+        onPress={() => {
+          alert("default");
+        }}
+      />
+
+      <Counter />
     </View>
   );
 };
--- react-native-component/src/components/MyButton.js
+++ react-native-component/src/components/MyButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
-const MyButton = () => {
+const MyButton = (props) => {
   return (
     <TouchableOpacity
       style={{
@@ -10,11 +10,19 @@ const MyButton = () => {
         margin: 10,
         borderRadius: 8,
       }}
-      onPress={() => alert("Click !")}
+      onPress={() => props.onPress()}
     >
-      <Text style={{ fontSize: 18 }}>My Button</Text>
+      <Text style={{ fontSize: 18, color: "white" }}>
+        {/* props에 children이 있다면 title보다 우선시되도록 작성 */}
+        {props.children || props.title}
+      </Text>
     </TouchableOpacity>
   );
+};
+
+// 컴포넌트에 사용될 기본 값을 입력하여 빈 값이 나타나는 것을 방지
+MyButton.defaultProps = {
+  title: "Button",
 };
 
 export default MyButton;
