7b1f6c30f55d48054e1a56c4679f2aa3a560a16e
ktyoung <ktyoung0507@gmail.com>
2023-09-05 14:22:28 +0900

    chap4-2. 리액트 네이티브 스타일

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { textStyles } from "./styles";
+import { View } from "react-native";
+import { viewStyles } from "./styles";
+import { Contents, Footer, Header } from "./components/Layout";
 
 const App = () => {
   return (
-    // 인라인 스타일링
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {/* 클래스 스타일링 */}
-      <Text style={[textStyles.text, { color: "green" }]}>
-        Inline Styling - Text
-      </Text>
-      {/* 여러 개의 클래스 스타일을 적용해야 할 때는 배열을 이용하기 */}
-      {/* 뒤에 오는 스타일이 앞에 있는 스타일을 덮어씀 */}
-      <Text style={[textStyles.text, textStyles.error]}>
-        Inline Styling - Error
-      </Text>
+    <View style={viewStyles.container}>
+      <Header />
+      <Contents />
+      <Footer />
     </View>
   );
 };
