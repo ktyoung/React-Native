4193c9d793a2b1303acbab786479ec62eda2c054
ktyoung <ktyoung0507@gmail.com>
2023-09-07 12:30:12 +0900

    chap6-2. useEffect

---
+++ react-native-hooks/src/components/Form.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components/native";
+
+const StyledTextInput = styled.TextInput.attrs({
+  autoCapitalize: "none",
+  autoCorrect: false,
+})`
+  border: 1px solid #757575;
+  padding: 10px;
+  margin: 10px 0px;
+  width: 200px;
+  font-size: 20px;
+`;
+
+const StyledText = styled.Text`
+  font-size: 24px;
+  margin: 10px;
+`;
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    console.log(`name: ${name}, email: ${email} \n`);
+    return () => console.log(`\n ===== Form Component Unmount ===== \n`);
+  }, []);
+
+  return (
+    <>
+      <StyledText>Name: {name}</StyledText>
+      <StyledText>Email: {email}</StyledText>
+      <StyledTextInput
+        value={name}
+        onChangeText={(text) => setName(text)}
+        placeholder="name"
+      />
+      <StyledTextInput
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        placeholder="email"
+      />
+    </>
+  );
+};
+
+export default Form;
